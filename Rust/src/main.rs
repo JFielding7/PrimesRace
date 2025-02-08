1dbece11690429a41dda2e42422870d64d7033c9
joe <josephfielding03@gmail.com>
2025-02-08 16:53:12 -0500

    split into functions

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -5,6 +5,33 @@ fn get_first_multiple(prime: usize, lower: usize) -> usize {
     max(prime * prime, prime * ((lower - 1) / prime + 1))
 }
 
+fn prime_sieve(primes: &Vec<usize>, prime_factor_limit: usize, lower: usize, upper: usize) -> Vec<bool> {
+    let mut is_prime = vec![true; upper - lower];
+
+    let mut i = 0;
+    while i < primes.len() && primes[i] < prime_factor_limit {
+        for multiple in (get_first_multiple(primes[i], lower)..upper).step_by(primes[i]) {
+            is_prime[multiple - lower] = false;
+        }
+        i += 1;
+    }
+
+    is_prime
+}
+
+fn append_primes(primes: &mut Vec<usize>, is_prime: &Vec<bool>, lower: usize, n: usize) -> bool {
+    for (i, &prime) in is_prime.iter().enumerate() {
+        if prime {
+            primes.push(i + lower);
+            if primes.len() == n {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
 fn first_n_primes(n: usize) -> Vec<usize> {
     let mut primes = Vec::with_capacity(n);
     if n > 0 {
@@ -20,25 +47,9 @@ fn first_n_primes(n: usize) -> Vec<usize> {
     let mut upper = 10;
 
     loop {
-        let mut is_prime = vec![true; range];
-
-        let mut i = 0;
-        while i < primes.len() && primes[i] < prime_factor_limit {
-            let prime = primes[i];
-            let first_multiple = get_first_multiple(prime, lower);
-            for multiple in (first_multiple..upper).step_by(prime) {
-                is_prime[multiple - lower] = false;
-            }
-            i += 1;
-        }
-
-        for (i, &prime) in is_prime.iter().enumerate() {
-            if prime {
-                primes.push(i + lower);
-                if primes.len() == n {
-                    return primes;
-                }
-            }
+        let is_prime = prime_sieve(&primes, prime_factor_limit, lower, upper);
+        if append_primes(&mut primes, &is_prime, lower, n) {
+            return primes;
         }
 
         prime_factor_limit += 1;
